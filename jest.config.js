f0b8ac87bd04b6acfce5656cb4b312305338648b
Pivneva <markeng@yandex.ru>
2024-05-01 14:51:31 +0300

    tests

--- jest.config.js
+++ jest.config.js
@@ -43,9 +43,9 @@ const config = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 95,
-      functions: 95,
-      lines: 95,
+      branches: 90,
+      functions: 90,
+      lines: 90,
       statements: -10,
     },
   },
